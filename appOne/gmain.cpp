211386d36aa89f01b44cc4c7320b30939febd0bd
Coding-Ocean <eijo524@gmail.com>
2021-03-26 13:57:05 +0900

    change acos to atan2

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,12 +1,15 @@
 #include"libOne.h"
 void gmain() {
+    //ベクトルａ，ｂ
     float ax, ay;
     float bx, by;
     float a_angle = -15;
     float b_angle = 15;
+    //内積、外積、なす角
     float cosAngle = 0;
     float sinAngle = 0;
     float angleBetween = 0;
+    //状態
     int state = 0;
     angleMode(DEGREES);
     window(1000, 1000);
@@ -23,24 +26,21 @@ void gmain() {
         if (isTrigger(MOUSE_RBUTTON))b_angle += -15;
         bx = cos(b_angle);
         by = sin(b_angle);
+        //ベクトルｂをマウス位置にする
         if (isPress(MOUSE_MBUTTON)) {
             bx = mathMouseX;
             by = mathMouseY;
-            float mag = sqrt(bx * bx + by * by);
-            bx /= mag;
-            by /= mag;
         }
         //内積
         cosAngle = ax * bx + ay * by;
-        angleBetween = acos(cosAngle);
         //外積
         sinAngle = ax * by - ay * bx;
-        if (sinAngle < 0) {
-            angleBetween = -angleBetween;
+        //なす角
+        angleBetween = atan2(sinAngle, cosAngle);
+        //ベクトルａがｂを追いかける
+        if (isPress(KEY_Z)) {
+            a_angle += angleBetween * 0.1f;
         }
-
-        //a_angle += angleBetween * 0.01f;
-        
         //ベクトルｂ描画
         strokeWeight(10);
         textSize(50);
@@ -58,6 +58,7 @@ void gmain() {
             ++state %= 4;
         }
         if (state == 1 || state == 2) {
+            //底辺と高さ描画
             float cx = ax * cosAngle;
             float cy = ay * cosAngle;
             strokeWeight(10);
