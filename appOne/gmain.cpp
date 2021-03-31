6f59b0b42f99632292dd2278f660381427909d54
Coding-Ocean <eijo524@gmail.com>
2021-03-31 20:09:29 +0900

    21-03-31 20:10

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,18 +1,18 @@
 #include"libOne.h"
 void gmain() {
+    window(1040, 1040);
+    angleMode(DEGREES);
     //ベクトルａ，ｂ
     float ax, ay;
     float bx, by;
     float a_angle = -15;
-    float b_angle = 15;
+    float b_angle = 30;
     //内積、外積、なす角
-    float cosAngle = 0;
-    float sinAngle = 0;
+    float dotProduct = 0;
+    float crossProduct = 0;
     float angleBetween = 0;
     //表示切り替え用状態
     int state = 0;
-    angleMode(DEGREES);
-    window(1000, 1000);
     while (notQuit) {
         clear(0, 50, 0);
         mathAxis(1.5f, 255);
@@ -22,8 +22,8 @@ void gmain() {
         ax = cos(a_angle);
         ay = sin(a_angle);
         //ベクトルｂの回転
-        if (isTrigger(KEY_J))b_angle += 15;
-        if (isTrigger(KEY_L))b_angle += -15;
+        if (isTrigger(KEY_LEFT))b_angle += 15;
+        if (isTrigger(KEY_RIGHT))b_angle += -15;
         bx = cos(b_angle);
         by = sin(b_angle);
         //ベクトルｂをマウス位置にする
@@ -32,11 +32,11 @@ void gmain() {
             by = mathMouseY;
         }
         //内積
-        cosAngle = ax * bx + ay * by;
+        dotProduct = ax * bx + ay * by;
         //外積
-        sinAngle = ax * by - ay * bx;
+        crossProduct = ax * by - ay * bx;
         //なす角
-        angleBetween = atan2(sinAngle, cosAngle);
+        angleBetween = atan2(crossProduct, dotProduct);
         //ベクトルａがｂを追いかける
         if (isPress(KEY_Z)) {
             a_angle += angleBetween * 0.1f;
@@ -60,8 +60,8 @@ void gmain() {
         }
         if (state >= 1) {
             //底辺と高さ描画
-            float cx = ax * cosAngle;
-            float cy = ay * cosAngle;
+            float cx = ax * dotProduct;
+            float cy = ay * dotProduct;
             strokeWeight(10);
             stroke(150, 190, 255);
             mathLine(0, 0, cx, cy);
@@ -71,14 +71,14 @@ void gmain() {
         if (state >= 2) {
             textSize(50);
             fill(150, 190, 255);
-            text((let)"cosθ = ax * bx + ay * by = " + cosAngle, 0, 50);
+            text((let)"内積: ax * bx + ay * by = " + dotProduct, 0, 50);
             fill(255, 190, 190);
-            text((let)"sinθ = ax * by - ay * bx = " + sinAngle, 0, 100);
+            text((let)"外積: ax * by - ay * bx = " + crossProduct, 0, 100);
         }
-        if (state == 3) {
+        if (state >= 3) {
             //なす角描画
             stroke(255, 255, 200);
-            mathArc(atan2(ay, ax), angleBetween, 0.1f);
+            mathArc(a_angle, angleBetween, 0.1f);
             textSize(50);
             fill(255,255,200);
             text((let)"θ = " + round(angleBetween), 0, 150);
